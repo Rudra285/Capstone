bb160a304080d36b47c36f18282b15bcdcfa88df
Jocelyn Odorizzi <jodorizzi@live.com>
2022-10-05 06:49:07 -0600

    update
    Moved files under templates and screens into folders based on personal or business. Updated the main.kv imports and include. Made bottom navigation on login pages to go back to startup page

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from kivy.app import App
+#Kivy 2.0.1
+#KivyMD 1.1.0
+from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
+from  kivy.core.window import Window
 
 
 class WindowManager(ScreenManager):
     pass
 
-class TestApp(App):
+#App window size
+Window.size = (700, 700)
+
+class TestApp(MDApp):
     def build(self):
-        return Builder.load_file('main.kv')  # this method can be eliminated if `main.kv` is renamed to `test.kv`
+        self.title = "MY GARAGE"
+        self.theme_cls.primary_palette = "Green"
+        self.theme_cls.accent_palette = 'Gray'
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_file('main.kv')
 
 if __name__ == '__main__':
     TestApp().run()
---
+++ screens/personal/personal_login.py
@@ -0,0 +1,5 @@
+from kivymd.uix.screen import MDScreen
+
+
+class PersonalLoginScreen(MDScreen):
+    pass
