2fa85d1cfa9764ad2ea38759998b85f63c7a9d0f
Rudra Patel <90656120+Rudra285@users.noreply.github.com>
2022-10-06 13:39:11 -0400

    Update business_login.py

--- screens/business/business_login.py
+++ screens/business/business_login.py
@@ -1,6 +1,7 @@
 from kivymd.uix.screen import MDScreen
 from kivy.properties import StringProperty
-
+import os
+import json
 
 class BusinessLoginScreen(MDScreen):
     email_prompt = StringProperty("Email")
@@ -21,8 +22,23 @@ class BusinessLoginScreen(MDScreen):
         #get the input for the password
         password = self.ids.business_login_password.text
         print(password)
+        
+        #Check JSON file for existing account
+        json_path = os.path.dirname(os.path.abspath("users.json")) + '/business.json'
+        with open(json_path, 'r') as b_users:
+        	user_data = json.load(b_users)
+        b_users.close()
+        
+        #Check email with corresponding password
+        checklist = user_data.get(email)
+        if checklist == None:
+        	print('No value')
+        else:
+        	check = checklist[0]
+        	if check == password:
+        		print('verified')
+        		app.root.current = 'business_home_screen'
+        	else:
+        		print('Account DNE')
 
         #TODO Make sure the email and password are not empty
-
-
-
