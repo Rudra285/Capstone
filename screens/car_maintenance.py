e08b104272f93a351f528cfee5c07fc39454d475
Rudra285 <rudra.pat3@gmail.com>
2022-12-06 01:13:51 -0500

    Can now export vehicle history to pdf

--- screens/car_maintenance.py
+++ screens/car_maintenance.py
@@ -6,6 +6,7 @@ from kivy.clock import Clock
 from kivymd.uix.boxlayout import MDBoxLayout
 from bigchaindb_driver import BigchainDB
 from datetime import datetime
+from reportlab.pdfgen import canvas
 
 class Content(MDBoxLayout):
     header = StringProperty("the name of the business that did maintenance")
@@ -20,13 +21,15 @@ class Maintenance(MDExpansionPanelThreeLine):
 class CarMaintenanceScreen(MDScreen):
 	
 	LastScreen = ''
-
+	vin = ''
+	
 	def __init__(self, **kwargs):
 		super(CarMaintenanceScreen, self).__init__(**kwargs)
 		Clock.schedule_once(self.on_start)
         
 	def load(self, vin, screen):
 		self.LastScreen = screen
+		self.vin = vin
 		bdb_root_url = 'https://test.ipdb.io'
 		bdb = BigchainDB(bdb_root_url)
 		
@@ -82,5 +85,84 @@ class CarMaintenanceScreen(MDScreen):
 		root.manager.get_screen(self.LastScreen).load()
 		self.ids.content_maintenance.clear_widgets()
 		app.root.current = self.LastScreen
+	
+	def export_pdf(self):
+		vin = self.vin
+		filename = 'vehicle_history_' + vin + '.pdf'
+		
+		bdb_root_url = 'https://test.ipdb.io'
+		bdb = BigchainDB(bdb_root_url)
+		
+		query = bdb.metadata.get(search = vin)
+		asset_query = bdb.assets.get(search = vin)
+		
+		sortingList = []
+		for entry in query:
+			sortingList.append(entry['metadata']['date'])
+		sortingList.sort(key=lambda date: datetime.strptime(date, "%b/%d/%Y %I:%M:%S %p"))
+
+		sortedQuery = []
+		for localdatetime in sortingList:
+			for entry in query:
+				if entry['metadata']['date'] == localdatetime:
+					sortedQuery.append(entry)
+		for item in sortedQuery:
+			try:
+				key = item['metadata']['mileage']
+			except:
+				item['metadata']['mileage'] = key
+		sortedQuery.reverse()
+		
+		my_canvas = canvas.Canvas(filename)
+		my_canvas.setLineWidth(.3)
+		my_canvas.setFont('Helvetica', 24)
+		my_canvas.drawString(30, 750, 'My Garage Vehicle History')
+		my_canvas.setFont('Helvetica', 12)
+		data = 'Vehicle Identification Number: ' + vin
+		my_canvas.drawString(30, 735, data)
+		data = 'Make: ' + asset_query[0]['data']['vehicle']['make']
+		my_canvas.drawString(30, 720, data)
+		data = 'Model: ' + asset_query[0]['data']['vehicle']['model']
+		my_canvas.drawString(30, 705, data)
+		data = 'Year: ' + asset_query[0]['data']['vehicle']['year']
+		my_canvas.drawString(30, 690, data)
+		my_canvas.drawString(30, 675, '')
+		my_canvas.drawString(30, 660, '')
+		space = 660
+		
+		for entry in sortedQuery:
+			owners = ''
+			for i in entry['metadata']['owner']:
+				owners = owners + i + ', '
+			owners = owners[:-2]
+
+			data = entry['metadata']['maintenance']
+			my_canvas.drawString(30, space, data)
+			space -= 15
+			data = 'Date: ' + entry['metadata']['date']
+			my_canvas.drawString(30, space, data)
+			space -= 15
+			data = 'Owner(s): ' + owners
+			my_canvas.drawString(30, space, data)
+			space -= 15
+			logType = entry['metadata']['type']
+			
+			if logType == 'maint':
+				company = entry['metadata']['company']
+				company_query = bdb.assets.get(search = company)
+				data = 'Mechanic: ' + company
+				my_canvas.drawString(30, space, data)
+				space -= 15
+				data = 'Phone: ' + company_query[0]['data']['Dealership']['Phone']
+				my_canvas.drawString(30, space, data)
+				space -= 15
+				
+			data = 'Vehicle Traveled: ' + entry['metadata']['mileage']
+			my_canvas.drawString(30, space, data)
+			space -= 15
+			my_canvas.drawString(30, space, '')
+			space -= 15
+			
+		my_canvas.save()
             
 
